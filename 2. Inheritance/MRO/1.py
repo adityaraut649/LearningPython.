53e9d5ba682c8439d6e63d47fd30dd1b00c3b072
adityaraut649 <vraut3468@gmail.com>
2026-04-07 16:26:33 +0530

    Adding mro

--- 2. Inheritance/MRO/1.py
+++ 2. Inheritance/MRO/1.py
@@ -88,7 +88,6 @@ c.show(10, 20)
 
 
 # super() with arguments and keyword arguments
-
 class A:
     def show(self, x, y=10):
         print("A")
@@ -109,3 +108,155 @@ c = C()
 c.show(10, 20)
 
 # A -> B -> C -> A  
+
+
+
+
+class A:
+    def show(self):
+        print("This is A")
+
+
+class B(A):
+    def show(self):
+        super().show()   # calling parent method
+        print("This is B")
+
+
+b = B()
+b.show()
+#This is A.  , This is B
+
+
+print("----------super() with Constructor (__init__)--------------")
+
+
+class A:
+    def __init__(self):
+        print("A constructor")
+
+
+class B(A):
+    def __init__(self):
+        print("B constructor")
+        super().__init__()
+
+
+b = B()
+
+
+print("----------Multiple Inheritance - methods calling--------------")
+
+
+class A:
+    def show(self):
+        print("A")
+
+
+class B(A):
+    def show(self):
+        print("B")
+        super().show()
+
+
+class C(A):
+    def show(self):
+        print("C")
+        super().show()
+
+
+class D(B, C):
+    def show(self):
+        print("D")
+        super().show()
+
+
+d = D() # D B C A
+d.show()
+
+
+class A:
+    def show(self):
+        print("This is A")
+
+
+class B(A):
+    def show(self):
+        print("This is B")
+
+
+class C(A):
+    def show(self):
+        print("This is C")
+
+
+class D(B, C):
+    pass
+
+
+# Creating an instance
+d = D()
+d.show()  # Output: This is B
+
+
+print(D.__mro__)
+
+
+
+print("----------------------------------------")
+
+
+class A:
+    def __init__(self):
+        print("A is initialized")
+
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print("B is initialized")
+
+
+class C(A):
+    def __init__(self):
+        super().__init__()
+        print("C is initialized")
+
+
+class D(B, C):
+    def __init__(self):
+        super().__init__()
+        print("D is initialized")
+
+
+# Creating an instance
+d = D()
+
+
+print("_------------------------------------")
+
+
+class A:
+    pass
+
+
+class B(A): 
+    pass
+class C(A): 
+    pass
+
+
+class D(B, C): 
+    pass
+class E(C): 
+    pass
+
+
+class F(D, E): 
+    pass
+
+
+print(F.mro())
+
+
+# F D B E C A Obj
